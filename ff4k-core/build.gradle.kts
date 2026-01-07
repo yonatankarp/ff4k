cd6aa1c3c409504b50d0de9d48740a2d71883683
Yonatan Karp-Rudin <yonvata@gmail.com>
2026-01-07 10:31:19 +0200

    Setup ff4k-core
    This change setting a new module to hold the ff4k core library that later on will be extended by other modules in the project

---
+++ ff4k-core/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("ff4k.multiplatform")
+    id("ff4k.publish")
+    id("ff4k.coverage")
+    id("ff4k.documentation")
+    alias(libs.plugins.kotlin.serialization)
+}
+//
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+        }
+
+        jvmMain.dependencies {
+            implementation(libs.slf4j.api)
+        }
+
+        commonTest.dependencies {
+//            implementation(project(":feature4k-test"))
+            implementation(libs.kotlinx.coroutines.test)
+        }
+    }
+}
+
+dependencies {
+    add("androidUnitTestImplementation", libs.robolectric)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    includeBuild("build-logic")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "ff4k"
+
+include(
+    "ff4k-cache",
+    "ff4k-core",
+    "ff4k-dsl",
+    "ff4k-test",
+    "ff4k-web",
+)
